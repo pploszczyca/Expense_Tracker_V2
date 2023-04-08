f2b8c30f1f5cc7599be4ba21b748397066306352
pploszczyca <piotrus.plo@gmail.com>
2023-04-08 15:22:54 +0200

    Some AutoCompleteOutlinedTextField refactor

--- app/src/main/java/com/example/expensetrackerv2/ui/common_components/auto_complite_text_field/AutoCompleteOutlinedTextField.kt
+++ app/src/main/java/com/example/expensetrackerv2/ui/common_components/auto_complite_text_field/AutoCompleteOutlinedTextField.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Title
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
@@ -11,8 +13,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
+import com.example.expensetrackerv2.extensions.filterIf
 import com.example.expensetrackerv2.ui.form.ui.ExpenseFormTextField
 
 @Composable
@@ -24,27 +29,21 @@ fun AutoCompleteOutlinedTextField(
     suggestionsInput: List<String> = emptyList(),
     keyboardOptions: KeyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
 ) {
-    var typedText by remember { mutableStateOf(value) }
-    val suggestions = suggestionsInput.filter {
-        it.contains(
-            typedText
-        )
+    val suggestions = suggestionsInput.filterIf(value.isNotEmpty()) {
+        it.contains(value)
     }
 
     Column(modifier = Modifier.fillMaxWidth()) {
         ExpenseFormTextField(
             value = value,
-            onValueChange = {
-                onValueChange(it)
-                typedText = it
-            },
+            onValueChange = onValueChange,
             icon = icon,
             label = label,
             keyboardOptions = keyboardOptions
         )
 
         DropdownMenu(
-            expanded = (typedText != "" && suggestions.isNotEmpty() && suggestions.filter { it == typedText }.size != 1),
+            expanded = (value.isNotEmpty() && suggestions.contains(value).not() && suggestions.isNotEmpty()),
             onDismissRequest = { },
             modifier = Modifier
                 .fillMaxWidth()
@@ -54,11 +53,22 @@ fun AutoCompleteOutlinedTextField(
             suggestions.forEach { suggestedText ->
                 DropdownMenuItem(
                     onClick = {
-                        typedText = suggestedText
                         onValueChange(suggestedText)
                     },
                     text = { Text(text = suggestedText) })
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AutoCompleteOutlinedTextFieldPreview() {
+    AutoCompleteOutlinedTextField(
+        value = "My Title",
+        onValueChange = {},
+        icon = Icons.Default.Title,
+        label = "Title",
+        suggestionsInput = listOf("Test")
+    )
 }

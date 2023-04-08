92e639870c7084089ee12415b28d611f319f577d
pploszczyca <piotrus.plo@gmail.com>
2023-04-08 16:44:34 +0200

    Added filterIf tests

---
+++ app/src/test/java/com/example/expensetrackerv2/extensions/ListExtensionsKtTest.kt
@@ -0,0 +1,38 @@
+package com.example.expensetrackerv2.extensions
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.data.forAll
+import io.kotest.matchers.shouldBe
+
+class ListExtensionsKtTest : BehaviorSpec({
+
+    val listOfStrings = listOf("test", "hello", "second hello")
+
+    Given("False condition") {
+        val condition = false
+
+        When("Want to filter") {
+            val result = listOfStrings.filterIf(condition) {
+                it == "hello"
+            }
+
+            Then("The list should be the same") {
+                result shouldBe listOfStrings
+            }
+        }
+    }
+
+    Given("True condition") {
+        val condition = true
+
+        When("Want to filter") {
+            val result = listOfStrings.filterIf(condition) {
+                it == "hello"
+            }
+
+            Then("The list should be filtered") {
+                result shouldBe listOf("hello")
+            }
+        }
+    }
+})

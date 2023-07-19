613652fab72b908506570d13915bf9bc188aed04
pploszczyca <piotrus.plo@gmail.com>
2023-07-19 17:56:02 +0200

    Changed get to by lazy evaluation

--- domain/src/main/java/com/github/pploszczyca/expensetrackerv2/domain/ExpenseSummary.kt
+++ domain/src/main/java/com/github/pploszczyca/expensetrackerv2/domain/ExpenseSummary.kt
@@ -3,43 +3,51 @@ package com.github.pploszczyca.expensetrackerv2.domain
 data class ExpenseSummary(
     val yearlyExpenses: List<YearlyExpense>,
 ) {
-    val totalIncome: Double get() =
+    val totalIncome: Double by lazy {
         yearlyExpenses.sumOf { it.totalIncome }
+    }
 
-    val totalOutgo: Double get() =
+    val totalOutgo: Double by lazy {
         yearlyExpenses.sumOf { it.totalOutgo }
+    }
 
 
     data class YearlyExpense(
         val year: Int,
         val monthlyExpenses: List<MonthlyExpense>,
     ) {
-        val totalIncome: Double get() =
+        val totalIncome: Double by lazy {
             monthlyExpenses.sumOf { it.totalIncome }
+        }
 
-        val totalOutgo: Double get() =
+        val totalOutgo: Double by lazy {
             monthlyExpenses.sumOf { it.totalOutgo }
+        }
 
 
         data class MonthlyExpense(
             val month: Int,
             val dailyExpenses: List<DailyExpense>,
         ) {
-            val totalIncome: Double get() =
+            val totalIncome: Double by lazy {
                 dailyExpenses.sumOf { it.totalIncome }
+            }
 
-            val totalOutgo: Double get() =
+            val totalOutgo: Double by lazy {
                 dailyExpenses.sumOf { it.totalOutgo }
+            }
 
             data class DailyExpense(
                 val day: Int,
                 val expenses: List<Expense>,
             ) {
-                val totalIncome: Double get() =
+                val totalIncome: Double by lazy {
                     expenses.totalIncome
+                }
 
-                val totalOutgo: Double get() =
+                val totalOutgo: Double by lazy {
                     expenses.totalOutgo
+                }
             }
         }
     }

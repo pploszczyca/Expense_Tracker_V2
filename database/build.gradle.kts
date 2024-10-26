01b8581abbd838039a3afef019e14bc284f5e78f
Piotr Płoszczyca <piotrus.plo@gmail.com>
2024-10-26 19:23:51 +0200

    Bumped Java to 21

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -14,12 +14,12 @@ android {
     namespace = "com.github.pploszczyca.expensetrackerv2.database"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 
     packaging {
--- features/expense-form/build.gradle.kts
+++ features/expense-form/build.gradle.kts
@@ -16,12 +16,12 @@ android {
     namespace = "com.github.pploszczyca.expensetrackerv2.features.expense_form"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 
     packaging {

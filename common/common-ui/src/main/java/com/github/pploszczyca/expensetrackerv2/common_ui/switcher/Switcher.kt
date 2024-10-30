bcac77f155a811cd5087c79dd48388937eb0f780
Piotr Płoszczyca <piotrus.plo@gmail.com>
2024-10-30 19:19:14 +0100

    Imports

--- common/common-ui/src/main/java/com/github/pploszczyca/expensetrackerv2/common_ui/switcher/Switcher.kt
+++ common/common-ui/src/main/java/com/github/pploszczyca/expensetrackerv2/common_ui/switcher/Switcher.kt
@@ -3,13 +3,9 @@ package com.github.pploszczyca.expensetrackerv2.common_ui.switcher
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
